a9ab2054bb70cfe5e916a129d22e84ac10382b0f
minkbear <minkbear@gmail.com>
2022-03-23 10:32:16 +0700

    fix bug

--- src/Vault.php
+++ src/Vault.php
@@ -14,10 +14,10 @@ class Vault
     private $vault;
 
     public function __construct(
-        string $tenant_id,
-        string $client_id,
-        string $client_secret,
-        string $vault
+        string $tenant_id = '',
+        string $client_id = '',
+        string $client_secret = '',
+        string $vault = ''
     ) {
         $this->tenant_id = $tenant_id;
         $this->client_id = $client_id;
